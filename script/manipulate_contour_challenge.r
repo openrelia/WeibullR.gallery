00561986db06defb3ec991f83c5dc3f5d2810322
Jacob Ormerod <jto888@users.noreply.github.com>
2019-03-07 23:45:28 -0500

    added contour challenge files

---
+++ script/manipulate_contour_challenge.r
@@ -0,0 +1,13 @@
+daf<-read.csv("https://raw.githubusercontent.com/openrelia/WeibullR.gallery/master/data/contour_challenge/daTEST.csv", header=T)$agc
+das<-read.csv("https://raw.githubusercontent.com/openrelia/WeibullR.gallery/master/data/contour_challenge/dasuspendedTEST.csv", header=T)$agc
+
+fdf<-as.data.frame(table(daf[,1]))
+ft<-as.numeric(levels(fdf[,1]))
+fq<-fdf[,2]
+sdf<-as.data.frame(table(das[,1]))
+st<-as.numeric(levels(sdf[,1]))
+sq<-sdf[,2]
+fail_edata<-data.frame(time=ft, event=rep(1,length(ft)), qty=fq)
+sus_edata<-data.frame(time=st, event=rep(0, length(st)), qty=sq)
+teq_frame<-rbind(fail_edata, sus_edata)
+teq_frame
