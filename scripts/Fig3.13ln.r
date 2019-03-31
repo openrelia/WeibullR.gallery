63c5bfd89e36fd3f02e0a69917dce66bb3b49933
Jacob Ormerod <jto888@users.noreply.github.com>
2019-03-31 19:26:55 -0400

    Update Fig3.13ln.r
    use the canvas option correctly

--- scripts/Fig3.13ln.r
+++ scripts/Fig3.13ln.r
@@ -2,5 +2,5 @@ require(WeibullR)
 F3.13da<-read.csv("https://raw.githubusercontent.com/openrelia/WeibullR.gallery/master/data/Figure3.13.csv", header=T)$F3.13da
 F3.13ln2 <- wblr.fit(wblr(F3.13da,label="Figure3.13"),
 dist="lognormal",col="magenta")
-plot(F3.13ln2,log="xy",
+plot(F3.13ln2,canvas="lognormal",
 main="Lognormal Plot") 
