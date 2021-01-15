c75b6d7621a5222a6b31dff47a1a4d723321ab1c
Jacob Ormerod <jto888@users.noreply.github.com>
2021-01-15 18:02:37 -0500

    Create complete2p.r

---
+++ scripts/complete2p.r
@@ -0,0 +1,27 @@
+# complete2p.r
+
+library(WeibullR)
+
+S<-20
+CI<-.8
+n=20
+set.seed(654321)
+
+unity_pts<-qweibull(getPPP(rweibull(n,1,1))$ppp,1,1)
+
+
+obj_list<-list()
+
+for(x in 1:S) {
+	sample<-rweibull(n,1,1)
+	obj_list[[x]]<-wblr(sample, col="transparent")
+	obj_list[[x]]<-wblr.fit(obj_list[[x]], col="gray", lwd=.1)
+}
+
+# graphic presentation
+obj<-wblr(unity_pts, col="transparent")
+obj<-wblr.fit(obj, col="black")
+obj<-wblr.conf(obj, ci=CI, lty="dashed", lwd=2, col="red")
+obj_list[[S+1]]<-obj
+
+plot.wblr(obj_list, is.plot.legend=FALSE, main="Pivotal Analysis of Complete Failures" )
